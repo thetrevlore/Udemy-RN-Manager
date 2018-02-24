a83303b6c73beaaff0aec4a885c492bd4de92f04
Trevor Storey <trevorstorey23@gmail.com>
2018-02-23 22:39:50 -0500

    [master] Redux/Firestore boilerplate finished, start auth - manager/*

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import FIREBASE_API_KEY from './secrets';
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: FIREBASE_API_KEY,
+      authDomain: 'manager-f5fad.firebaseapp.com',
+      databaseURL: 'https://manager-f5fad.firebaseio.com',
+      projectId: 'manager-f5fad',
+      storageBucket: '',
+      messagingSenderId: '175690471406'
+    };
+
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Heloo!</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
