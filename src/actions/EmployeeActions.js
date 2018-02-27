d15fcbe2ba1645431f2d3b0f2ea439e306244e89
Trevor Storey <trevorstorey23@gmail.com>
2018-02-26 23:33:21 -0500

    [master] completed the course

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,6 +1,10 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEES_FETCH_SUCCESS, EMPLOYEE_SAVE_SUCCESS } from './types';
+import {
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS,
+  EMPLOYEE_SAVE_SUCCESS } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
@@ -41,6 +45,18 @@ export const employeeSave = ({ name, phone, shift, uid }) => {
       .then(() => {
         dispatch({ type: EMPLOYEE_SAVE_SUCCESS });
         Actions.employeeList({ type: 'reset' });
+      });
+  };
+};
+
+export const employeeDelete = ({ uid }) => {
+  const { currentUser } = firebase.auth();
+
+  return () => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+      .remove()
+      .then(() => {
+        Actions.main({ type: 'reset' });
       });
   };
 };
--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,12 +1,14 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Communications from 'react-native-communications';
-import { employeeUpdate, employeeSave } from '../actions';
+import { text } from 'react-native-communications';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
 import EmployeeForm from './EmployeeForm';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
+  state = { showModal: false };
+
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value });
@@ -20,10 +22,20 @@ class EmployeeEdit extends Component {
 
   onTextPress() {
     const { phone, shift } = this.props;
-    Communications.text(phone, `Your upcoming shift is on ${shift}`);
+    text(phone, `Your upcoming shift is on ${shift}`);
+  }
+
+  onDecline() {
+    this.setState({ showModal: false });
+  }
+
+  onAccept() {
+    const { uid } = this.props.employee;
+    this.props.employeeDelete({ uid });
   }
 
   render() {
+    const { name } = this.props.employee;
     return (
       <Card>
         <EmployeeForm />
@@ -34,9 +46,23 @@ class EmployeeEdit extends Component {
         </CardSection>
         <CardSection>
           <Button onPress={this.onTextPress.bind(this)}>
-            {`Text ${this.props.employee.name}`}
+            {`Text ${name}`}
           </Button>
         </CardSection>
+
+        <CardSection>
+          <Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+            {`Fire ${name}`}
+          </Button>
+        </CardSection>
+
+        <Confirm
+          visible={this.state.showModal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}
+        >
+          {`Are you sure you want to fire ${name}?`}
+        </Confirm>
       </Card>
     );
   }
@@ -47,4 +73,5 @@ const mapStateToProps = (state) => {
   return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
+export default connect(mapStateToProps,
+  { employeeUpdate, employeeSave, employeeDelete })(EmployeeEdit);
